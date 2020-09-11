d0f0447a248b66d3fe8f1aba82388216e470d642
lihereshma <reshmalihe00068@gmail.com>
2020-09-11 14:44:44 +0530

    Updates

---
+++ precache-manifest.afc31142c84726d2e941c42bea58ff13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cb70417d4b414443530081d37cc3a0a",
+    "url": "/charts-react/index.html"
+  },
+  {
+    "revision": "d8c7671c407542ffc954",
+    "url": "/charts-react/static/css/main.aa97c5bf.chunk.css"
+  },
+  {
+    "revision": "7e67fc928dd4ea62a9e6",
+    "url": "/charts-react/static/js/2.4c744030.chunk.js"
+  },
+  {
+    "revision": "4db0b4b7fd2c7a0aaf4845dcd6ed7f11",
+    "url": "/charts-react/static/js/2.4c744030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8c7671c407542ffc954",
+    "url": "/charts-react/static/js/main.3773bca1.chunk.js"
+  },
+  {
+    "revision": "6e0482944de79c8f9cb9",
+    "url": "/charts-react/static/js/runtime-main.768ded96.js"
+  }
+]);
